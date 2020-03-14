fb018723d77f77fcf31c2198e5a2f1722c9fe037
unknown <billbrown20000@gmail.com>
2020-03-14 18:23:43 -0400

    homework complete

---
+++ src/main/java/shapetester.java
@@ -0,0 +1,37 @@
+public class shapetester {
+    public static void main(String[] args) {
+
+class square{
+    public void rotate(){
+        System.out.println("The square will rotate at about 90 degrees");
+    }
+       public void playSound(){
+           System.out.println("The square will play its sound");
+       }
+        }
+class triangle{
+    public void rotate(){
+        System.out.println("the triangle will rotate at 35 degrees");
+    }
+    public void playSound (){
+        System.out.println("the triangle will play its sound");
+    }
+}
+class circle{
+    public void rotate(){
+        System.out.println("the circle will rotate at 75 degrees");
+    }
+    public void playSound(){
+        System.out.println("the circle will play its sound");
+    }
+}
+class amoeba{
+    public void rotate(){
+        System.out.println("the amoeba will rotate at 80 degrees");
+    }
+    public void playSound(){
+        System.out.println("the amoeba will play its sound");
+    }
+}
+    }
+}
